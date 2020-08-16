7ce4ae6254b1f67f7ce8fb726a8f87319caa8f7c
yonekurayuuki <netjobcareers@gmail.com>
2020-08-16 14:19:07 +0900

    fixed

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const cheerio = require('cheerio');
 
+var h1 = 0
+var h2 = 0
+var h3 = 0
+
 module.exports = {
   hooks: {
     "page": function(page) {
-      $('h1').each(function (i, h1)){
-        $(h1).before('<span>hogehoge</span>')
-      }
+      $ = cheerio.load(page.content);
+      $('h1, h2, h3').each(function(i, h){
+        if($(h).get(0).tagName == 'h1'){
+          h1 += 1;
+          h2 = 0;
+          h3 = 0;
+          $('<span style="visibility: hidden;" class="book-chapter book-chapter-1">' + h1 + '. ' +$(h).text() + '</span>').insertBefore($(h));
+        }
+
+        if($(h).get(0).tagName == 'h2'){
+          h2 += 1;
+          h3 = 0;
+          $('<span style="visibility: hidden;" class="book-chapter book-chapter-2">' + h1 + '.' + h2 + '. ' +$(h).text() + '</span>').insertBefore($(h));
+        }
+
+        if($(h).get(0).tagName == 'h3'){
+          h3 += 1;
+          $('<span style="visibility: hidden;" class="book-chapter-3">' + h1 + '.' + h2 + '.' +h3 + '. ' + $(h).text() + '</span>').insertBefore($(h));
+        }
+
+      });
+      page.content = $.html();
+      return page;
     }
   }
 }
