35a9282dcc54a573723a61f9c2a2ba7aea2afc84
yonekurayuuki <netjobcareers@gmail.com>
2020-09-26 14:52:56 +0900

    fixed

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const cheerio = require('cheerio');
 
-var h1 = 0
-var h2 = 0
-var h3 = 0
-var h4 = 0
+var h1 = 0;
+var h2 = 0;
+var h3 = 0;
+var h4 = 0;
+var depth;
 
 module.exports = {
+  book: {},
   hooks: {
     "page": function(page) {
+      if(this.options.pluginsConfig['depth'] !== undefined)
+        depth = this.options.pluginsConfig['depth'];
+      else
+        depth = 0;
       $ = cheerio.load(page.content);
       $('h1, h2, h3, h4').each(function(i, h){
         if($(h).get(0).tagName == 'h1'){
@@ -15,28 +21,28 @@ module.exports = {
           h2 = 0;
           h3 = 0;
           h4 = 0;
-          $('<span style="visibility: hidden;" class="book-chapter book-chapter-1">' + h1 + '. ' +$(h).text() + '</span>').insertBefore($(h));
+          $('<span style="visibility: hidden; display: none;" class="book-chapter book-chapter-1">' + h1 + '. ' +$(h).text() + '</span>').insertBefore($(h));
         }
-        if(this.options.pluginsConfig['depth'] === 1) return true;
+        if(depth === 1) return true;
 
         if($(h).get(0).tagName == 'h2'){
           h2 += 1;
           h3 = 0;
           h4 = 0;
-          $('<span style="visibility: hidden;" class="book-chapter book-chapter-2">' + h1 + '.' + h2 + '. ' +$(h).text() + '</span>').insertBefore($(h));
+          $('<span style="visibility: hidden; display: none;" class="book-chapter book-chapter-2">' + h1 + '.' + h2 + '. ' +$(h).text() + '</span>').insertBefore($(h));
         }
-        if(this.options.pluginsConfig['depth'] === 2) return true;
+        if(depth === 2) return true;
 
         if($(h).get(0).tagName == 'h3'){
           h3 += 1;
           h4 = 0;
-          $('<span style="visibility: hidden;" class="book-chapter-3">' + h1 + '.' + h2 + '.' +h3 + '. ' + $(h).text() + '</span>').insertBefore($(h));
+          $('<span style="visibility: hidden; display: none;" class="book-chapter-3">' + h1 + '.' + h2 + '.' +h3 + '. ' + $(h).text() + '</span>').insertBefore($(h));
         }
-        if(this.options.pluginsConfig['depth'] === 3) return true;
+        if(depth === 3) return true;
 
         if($(h).get(0).tagName == 'h4'){
           h4 += 1;
-          $('<span style="visibility: hidden;" class="book-chapter-4">' + h1 + '.' + h2 + '.' +h3 + '.' +h4 + '. '+ $(h).text() + '</span>').insertBefore($(h));
+          $('<span style="visibility: hidden; display: none;" class="book-chapter-4">' + h1 + '.' + h2 + '.' +h3 + '.' +h4 + '. '+ $(h).text() + '</span>').insertBefore($(h));
         }
       });
       page.content = $.html();
